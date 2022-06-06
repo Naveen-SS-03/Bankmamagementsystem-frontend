a657c65a55c10c70af47fc0933ccb04f8da230fe
Naveen <104629665+Naveen-SS-03@users.noreply.github.com>
2022-06-06 13:11:14 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,21 +27,21 @@ const routes: Routes = [
   {path : 'adminRegistration',component:AdminRegistrationComponent},
   {path : 'dashboard',component:DashboardComponent},
 
-  {path: `dashboard/Account`, component: AccountListComponent},
-  {path: `dashboard/Account/Account/create`, component: CreateAccountComponent},
-  {path: '', redirectTo:`dashboard/Account/Account/getall`,pathMatch:'full'},
+  {path: `Account`, component: AccountListComponent},
+  {path: `Account/Account/create`, component: CreateAccountComponent},
+  {path: '', redirectTo:`Account/Account/getall`,pathMatch:'full'},
   
-  {path: `dashboard/Transaction`, component: TransactionListComponent},
-  {path: `dashboard/Transaction/Transaction/create`, component: CreateTransactionComponent},
-  {path: '', redirectTo:`dashboard/Transaction/Transaction/getall`,pathMatch:'full'},
+  {path: `Transaction`, component: TransactionListComponent},
+  {path: `Transaction/Transaction/create`, component: CreateTransactionComponent},
+  {path: '', redirectTo:`Transaction/Transaction/getall`,pathMatch:'full'},
 
-  {path: `dashboard/Recipient`, component: RecipientListComponent},
-  {path: `dashboard/Recipient/Recipient/create`, component: CreateRecipientComponent},
-  {path: '', redirectTo:`dashboard/Recipient/Recipient/getall`,pathMatch:'full'},
+  {path: `Recipient`, component: RecipientListComponent},
+  {path: `Recipient/Recipient/create`, component: CreateRecipientComponent},
+  {path: '', redirectTo:`Recipient/Recipient/getall`,pathMatch:'full'},
   
-  {path: `dashboard/Appointment`, component: AppoinmentListComponent},
-  {path: `dashboard/Appointment/Appointment/create`, component: CreateAppoinmentComponent},
-  {path: '', redirectTo:`dashboard/Appoinment/Appointment/getall`,pathMatch:'full'}
+  {path: `Appointment`, component: AppoinmentListComponent},
+  {path: `Appointment/Appointment/create`, component: CreateAppoinmentComponent},
+  {path: '', redirectTo:`Appoinment/Appointment/getall`,pathMatch:'full'}
 ];
 
 @NgModule({
