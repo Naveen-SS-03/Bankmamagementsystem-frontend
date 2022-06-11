b39e0fad9fd8decc2bee67dc6edb9ef5ce5b6b87
Naveen-SS-03 <naveenshan4546@gmail.com>
2022-06-11 09:44:27 +0530

    second commit

--- src/app/Account/account.ts
+++ src/app/Account/account.ts
@@ -2,6 +2,10 @@ export class Account {
     id:number | undefined;
 	accountholdername:String | undefined;
     accounttype:String | undefined;
+    accountholderage:number| undefined;
+    address:String | undefined;
+    pancardnumber:String | undefined;
+    nationality:String | undefined;
 	accountnumber:number | undefined;
     accountBalance:number | undefined;
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,31 +17,28 @@ import { TransactionListComponent } from './Transaction/transaction-list/transac
 
 
 const routes: Routes = [
-  {path:'',
-  redirectTo:'login',
-  pathMatch:'full'},
-  {path : 'login',
-  component:LoginComponent},
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path : 'login',component:LoginComponent},
   {path : 'AdminLogin',component:AdminLoginComponent},
   {path : 'registration',component:RegistrationComponent},
   {path : 'adminRegistration',component:AdminRegistrationComponent},
   {path : 'dashboard',component:DashboardComponent},
 
-  {path: `dashboard/Account`, component: AccountListComponent},
-  {path: `dashboard/Account/Account/create`, component: CreateAccountComponent},
-  {path: '', redirectTo:`dashboard/Account/Account/getall`,pathMatch:'full'},
+  {path: `Account`, component: AccountListComponent},
+  {path: `Account/Account/create`, component: CreateAccountComponent},
+  {path: '', redirectTo:`Account/Account/getall`,pathMatch:'full'},
   
-  {path: `dashboard/Transaction`, component: TransactionListComponent},
-  {path: `dashboard/Transaction/Transaction/create`, component: CreateTransactionComponent},
-  {path: '', redirectTo:`dashboard/Transaction/Transaction/getall`,pathMatch:'full'},
+  {path: `Transaction`, component: TransactionListComponent},
+  {path: `Transaction/Transaction/create`, component: CreateTransactionComponent},
+  {path: '', redirectTo:`Transaction/Transaction/getall`,pathMatch:'full'},
 
-  {path: `dashboard/Recipient`, component: RecipientListComponent},
-  {path: `dashboard/Recipient/Recipient/create`, component: CreateRecipientComponent},
-  {path: '', redirectTo:`dashboard/Recipient/Recipient/getall`,pathMatch:'full'},
+  {path: `Recipient`, component: RecipientListComponent},
+  {path: `Recipient/Recipient/create`, component: CreateRecipientComponent},
+  {path: '', redirectTo:`Recipient/Recipient/getall`,pathMatch:'full'},
   
-  {path: `dashboard/Appointment`, component: AppoinmentListComponent},
-  {path: `dashboard/Appointment/Appointment/create`, component: CreateAppoinmentComponent},
-  {path: '', redirectTo:`dashboard/Appoinment/Appointment/getall`,pathMatch:'full'}
+  {path: `Appointment`, component: AppoinmentListComponent},
+  {path: `Appointment/Appointment/create`, component: CreateAppoinmentComponent},
+  {path: '', redirectTo:`Appoinment/Appointment/getall`,pathMatch:'full'}
 ];
 
 @NgModule({
